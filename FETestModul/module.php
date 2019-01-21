c0d6756aeff020e98273b5d9b30abda0fea19b9e
DonCri <36209368+DonCri@users.noreply.github.com>
2019-01-21 22:11:51 +0100

    Add files via upload

--- FETestModul/module.php
+++ FETestModul/module.php
@@ -39,7 +39,7 @@
         public function MeineErsteEigeneFunktion() {
             // Selbsterstellter Code
         }
-    }
+
 
 public function ReceiveData($JSONString) {
  
@@ -51,5 +51,8 @@ public function ReceiveData($JSONString) {
     SetValue($this->GetIDForIdent("Value"), $data->Buffer);
  
 }
+
+    }
+
 
 ?>
